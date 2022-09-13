9a372fefa88ba814ceb8416164a811840612ce95
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-13 11:49:52 -0400

    AutoWiring

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Dimensions.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Dimensions.java
@@ -7,12 +7,17 @@ import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @Component
 public class Dimensions {
     private int width;
     private int height;
     private int depth;
+
+    public Dimensions() {
+        this.width=10;
+        this.height=30;
+        this.depth=40;
+    }
 
     public void pressPowerButton() {
         System.out.println("Power button press");
