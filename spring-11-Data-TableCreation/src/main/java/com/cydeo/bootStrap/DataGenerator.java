08b62fa86423e5328b07bd1eeb292aef7f758178
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-23 16:11:28 -0400

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/bootStrap/DataGenerator.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/bootStrap/DataGenerator.java
@@ -1,29 +1,65 @@
 package com.cydeo.bootStrap;
 
 import com.cydeo.entity.Car;
+import com.cydeo.entity.Department;
+import com.cydeo.entity.Employee;
+import com.cydeo.enums.Gender;
 import com.cydeo.repository.CarRepository;
+import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeesRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
     private final CarRepository carRepository;
+    private final EmployeesRepository employeesRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public DataGenerator(CarRepository carRepository) {
+    public DataGenerator(CarRepository carRepository, EmployeesRepository employeesRepository, DepartmentRepository departmentRepository) {
         this.carRepository = carRepository;
+        this.employeesRepository = employeesRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        Car c1 = new Car("BMW", "M3" );
-        Car c2 = new Car("Honda", "Civic" );
-        Car c3 = new Car("Toyota", "Corolla" );
+        Car c1 = new Car("BMW", "M3");
+        Car c2 = new Car("Honda", "Civic");
+        Car c3 = new Car("Toyota", "Corolla");
 
         carRepository.save(c1);
         carRepository.save(c2);
         carRepository.save(c3);
+
+        Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006, 04, 20), Gender.FEMALE, 2500);
+        Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009, 01, 26), Gender.FEMALE, 2500);
+        Employee e3 = new Employee("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010, 05, 17), Gender.FEMALE, 2500);
+        Employee e4 = new Employee("Avrom", "Rowantree", null, LocalDate.of(2014, 03, 02), Gender.MALE, 2500);
+        Employee e5 = new Employee("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003, 01, 14), Gender.MALE, 2500);
+
+        Department d1 = new Department("Sports", "Outdoors");
+        Department d2 = new Department("Tools", "Hardware");
+        Department d3 = new Department("Clothing", "Home");
+        Department d4 = new Department("Phones & Tablets", "Electronics");
+        Department d5 = new Department("Computers", "Electronics");
+
+        employeesRepository.save(e1);
+        employeesRepository.save(e2);
+        employeesRepository.save(e3);
+        employeesRepository.save(e4);
+        employeesRepository.save(e5);
+
+        departmentRepository.save(d1);
+        departmentRepository.save(d2);
+        departmentRepository.save(d3);
+        departmentRepository.save(d4);
+        departmentRepository.save(d5);
+
 
     }
 }
---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Department.java
@@ -0,0 +1,16 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department extends BaseIdentity{
+    private String department;
+    private String division;
+
+}
