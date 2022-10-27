f83d6f3c356163e7cd0180fa4ee885db5bda7fb2
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-27 12:40:40 -0400

    Spring Data Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -0,0 +1,24 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+public interface RegionRepository extends JpaRepository<Region,Integer> {
+
+    //Display all region in Canada
+    List<Region> findByCountry(String country);
+    List<Region> getByCountry(String country);
+
+    //Display all region with country name includes 'United'
+    List<Region> findByCountryContaining(String country);
+
+    //Display all region with country name includes 'United' in order(region)
+    List<Region> findByCountryContainsOrderByRegionDesc(String country);
+
+    //Display top 2 regions in United States
+    List<Region> findTopByCountry(String country);
+    List<Region> findTop2ByCountry(String country);
+}
