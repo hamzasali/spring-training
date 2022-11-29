3196c502b6528d97cf8ae81b343e290f910001f1
Hamza Ali <hamzamughal2145@gmail.com>
2022-11-29 14:10:37 -0500

    Spring REST School Lab

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -54,12 +55,17 @@ public class SchoolController {
 
 
     @GetMapping("/address/{id}")
-    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id") long id) throws Exception {
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id") Long id) throws Exception {
 
         AddressDTO addressDTO = addressService.findById(id);
         return ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved", addressDTO));
     }
 
+    @GetMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id") Long id, @RequestBody AddressDTO addressDTO) throws Exception {
+        addressDTO.setId(id);
+        return addressService.update(addressDTO);
+    }
 
 
 }
