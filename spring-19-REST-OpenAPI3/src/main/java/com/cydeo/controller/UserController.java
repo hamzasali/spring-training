e6b3e2ff3428768dedc160822fa9aae7734a184c
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-01 15:25:37 -0500

    Spring REST OpenAPI

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "")
 public class UserController {
 
     private final UserService userService;
