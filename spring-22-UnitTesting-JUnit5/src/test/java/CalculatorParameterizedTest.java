58964fd7e5c847ab69a5035ee7ba0b73c94ae2fa
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-15 15:24:06 -0500

    UnitTesting/Parameterized testing

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,17 +1,38 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class CalculatorParameterizedTest {
     @ParameterizedTest
     @ValueSource(strings = {"Java", "JS", "TS"})
-    void testCase(String arg) {
+    void testCase1(String arg) {
         Assertions.assertFalse(arg.isEmpty());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9})
-    void testCase(int num) {
+    void testCase2(int num) {
         Assertions.assertEquals(0, num % 3);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "JS", "TS"})
+//    @EmptySource
+//    @NullSource
+    @NullAndEmptySource
+    void testCase3(String arg) {
+        Assertions.assertFalse(arg.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringProvider")
+    void testCase4(String arg) {
+        Assertions.assertFalse(arg.isEmpty());
+    }
+
+    static String[] stringProvider() {
+        return new String[]{"Java", "JS", "TS"};
+    }
+
+
 }
