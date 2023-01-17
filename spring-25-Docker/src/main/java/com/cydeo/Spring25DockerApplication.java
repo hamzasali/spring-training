62c67c38607b57748600d3c13e9945dbbb10422b
Hamza Ali <hamzamughal2145@gmail.com>
2023-01-17 15:25:27 -0500

    Docker

--- spring-25-Docker/src/main/java/com/cydeo/Spring25DockerApplication.java
+++ spring-25-Docker/src/main/java/com/cydeo/Spring25DockerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring24DockerApplication {
+public class Spring25DockerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring24DockerApplication.class, args);
+        SpringApplication.run(Spring25DockerApplication.class, args);
     }
 
 }
--- spring-25-Docker/src/test/java/com/cydeo/Spring25DockerApplicationTests.java
+++ spring-25-Docker/src/test/java/com/cydeo/Spring25DockerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring24DockerApplicationTests {
+class Spring25DockerApplicationTests {
 
     @Test
     void contextLoads() {
